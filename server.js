b5ccbc3b909fa471be0120bbb243b950d0c0f4e3
Steve Brown <contactstevebrown@gmail.com>
2012-12-17 00:19:42 +0000

    another kind of encoding

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function init() {
                     function (stream) {
                         console.log('Connected to Stream API!');
                         stream.on('data', function (data) {
-                            tweetArray.push(strencode(data));
+                            tweetArray.push(strdecode(data));
                         });
 
                         fs.readFile('anew.json', 'utf8', function (fileDataErr, fileData) {
